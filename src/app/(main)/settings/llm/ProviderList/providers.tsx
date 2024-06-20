02dd469e20209da8caece053a46863e5121e98ed
Finsi19 <zfino52@gmail.com>
2024-06-20 18:00:24 +0700

    8

--- src/app/(main)/settings/llm/ProviderList/providers.tsx
+++ src/app/(main)/settings/llm/ProviderList/providers.tsx
@@ -1,20 +1,18 @@
 import {
-  Anthropic,
-  Claude,
-  DeepSeek,
+  // Anthropic,
+  // Claude,
+  // DeepSeek,
   Gemini,
   Google,
-  Groq,
-  Minimax,
-  Mistral,
-  Moonshot,
-  OpenRouter,
-  Perplexity,
-  Stepfun,
-  Together,
-  Tongyi,
-  ZeroOne,
-  Zhipu,
+  Groq, // Minimax,
+  // Mistral,
+  // Moonshot,
+  // OpenRouter,
+  // Perplexity,
+  // Stepfun,
+  Together, // Tongyi,
+  // ZeroOne,
+  // Zhipu,
 } from '@lobehub/icons';
 import { Divider } from 'antd';
 import { useTheme } from 'antd-style';
@@ -22,42 +20,40 @@ import { useMemo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
 import {
-  AnthropicProviderCard,
-  DeepSeekProviderCard,
+  // AnthropicProviderCard,
+  // DeepSeekProviderCard,
   GoogleProviderCard,
-  GroqProviderCard,
-  MinimaxProviderCard,
-  MistralProviderCard,
-  MoonshotProviderCard,
-  OpenRouterProviderCard,
-  PerplexityProviderCard,
-  QwenProviderCard,
-  StepfunProviderCard,
-  TogetherAIProviderCard,
-  ZeroOneProviderCard,
-  ZhiPuProviderCard,
+  GroqProviderCard, // MinimaxProviderCard,
+  // MistralProviderCard,
+  // MoonshotProviderCard,
+  // OpenRouterProviderCard,
+  // PerplexityProviderCard,
+  // QwenProviderCard,
+  // StepfunProviderCard,
+  TogetherAIProviderCard, // ZeroOneProviderCard,
+  // ZhiPuProviderCard,
 } from '@/config/modelProviders';
 
 import { ProviderItem } from '../type';
-import { useAzureProvider } from './Azure';
+// import { useAzureProvider } from './Azure';
 import { useBedrockProvider } from './Bedrock';
 // import { useOllamaProvider } from './Ollama';
 import { useOpenAIProvider } from './OpenAI';
 
-const AnthropicBrand = () => {
-  const { isDarkMode } = useTheme();
-  return <Anthropic.Text color={isDarkMode ? undefined : Claude.colorPrimary} size={15} />;
-};
+// const AnthropicBrand = () => {
+//   const { isDarkMode } = useTheme();
+//   return <Anthropic.Text color={isDarkMode ? undefined : Claude.colorPrimary} size={15} />;
+// };
 
-const MoonshotBrand = () => {
-  const theme = useTheme();
-  return (
-    <Moonshot.Combine
-      color={theme.isDarkMode ? theme.colorText : Moonshot.colorPrimary}
-      size={22}
-    />
-  );
-};
+// const MoonshotBrand = () => {
+//   const theme = useTheme();
+//   return (
+//     <Moonshot.Combine
+//       color={theme.isDarkMode ? theme.colorText : Moonshot.colorPrimary}
+//       size={22}
+//     />
+//   );
+// };
 
 const GroqBrand = () => {
   const theme = useTheme();
@@ -74,7 +70,7 @@ const GoogleBrand = () => (
 );
 
 export const useProviderList = (): ProviderItem[] => {
-  const azureProvider = useAzureProvider();
+  // const azureProvider = useAzureProvider();
   // const ollamaProvider = useOllamaProvider();
   const openAIProvider = useOpenAIProvider();
   const bedrockProvider = useBedrockProvider();
@@ -83,65 +79,65 @@ export const useProviderList = (): ProviderItem[] => {
     () => [
       openAIProvider,
       // ollamaProvider,
-      azureProvider,
+      // azureProvider,
       {
         ...GoogleProviderCard,
         title: <GoogleBrand />,
       },
-      {
-        ...AnthropicProviderCard,
-        title: <AnthropicBrand />,
-      },
+      // {
+      //   ...AnthropicProviderCard,
+      //   title: <AnthropicBrand />,
+      // },
       bedrockProvider,
       {
         ...GroqProviderCard,
         title: <GroqBrand />,
       },
-      {
-        ...OpenRouterProviderCard,
-        title: <OpenRouter.Combine iconProps={{ color: OpenRouter.colorPrimary }} size={20} />,
-      },
+      // {
+      //   ...OpenRouterProviderCard,
+      //   title: <OpenRouter.Combine iconProps={{ color: OpenRouter.colorPrimary }} size={20} />,
+      // },
       {
         ...TogetherAIProviderCard,
         title: <Together.Combine size={26} type={'color'} />,
       },
-      {
-        ...QwenProviderCard,
-        title: <Tongyi.Combine extra={'千问'} size={26} type={'color'} />,
-      },
-      {
-        ...DeepSeekProviderCard,
-        title: <DeepSeek.Combine size={28} type={'color'} />,
-      },
-      {
-        ...MinimaxProviderCard,
-        title: <Minimax.Combine size={32} type={'color'} />,
-      },
-      {
-        ...MistralProviderCard,
-        title: <Mistral.Combine size={26} type={'color'} />,
-      },
-      {
-        ...MoonshotProviderCard,
-        title: <MoonshotBrand />,
-      },
-      {
-        ...PerplexityProviderCard,
-        title: <Perplexity.Combine size={24} type={'color'} />,
-      },
-      {
-        ...ZhiPuProviderCard,
-        title: <Zhipu.Combine size={32} type={'color'} />,
-      },
-      {
-        ...ZeroOneProviderCard,
-        title: <ZeroOne.Text size={20} />,
-      },
-      {
-        ...StepfunProviderCard,
-        title: <Stepfun.Combine size={20} type={'color'} />,
-      },
+      // {
+      //   ...QwenProviderCard,
+      //   title: <Tongyi.Combine extra={'千问'} size={26} type={'color'} />,
+      // },
+      // {
+      //   ...DeepSeekProviderCard,
+      //   title: <DeepSeek.Combine size={28} type={'color'} />,
+      // },
+      // {
+      //   ...MinimaxProviderCard,
+      //   title: <Minimax.Combine size={32} type={'color'} />,
+      // },
+      // {
+      //   ...MistralProviderCard,
+      //   title: <Mistral.Combine size={26} type={'color'} />,
+      // },
+      // {
+      //   ...MoonshotProviderCard,
+      //   title: <MoonshotBrand />,
+      // },
+      // {
+      //   ...PerplexityProviderCard,
+      //   title: <Perplexity.Combine size={24} type={'color'} />,
+      // },
+      // {
+      //   ...ZhiPuProviderCard,
+      //   title: <Zhipu.Combine size={32} type={'color'} />,
+      // },
+      // {
+      //   ...ZeroOneProviderCard,
+      //   title: <ZeroOne.Text size={20} />,
+      // },
+      // {
+      //   ...StepfunProviderCard,
+      //   title: <Stepfun.Combine size={20} type={'color'} />,
+      // },
     ],
-    [azureProvider, bedrockProvider],
+    [bedrockProvider],
   );
 };
